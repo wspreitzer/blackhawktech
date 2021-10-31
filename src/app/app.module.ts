65da22ad314a4bb3b09b08d1914b0b02bf2bbcfe
Bill Spreitzer <you@example.com>
2021-10-31 05:49:36 -0500

    fixing missed file changes from menu component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './menu/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
